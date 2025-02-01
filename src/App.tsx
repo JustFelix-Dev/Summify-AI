d6f28a953b230f20522f4e7aa0335ee42ab87873
Felix <owolabifelix78@gmail.com>
2025-02-01 10:10:32 +0100

    added assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     
+      <main>
+      <div className='main'>
+        <div className='gradient' />
+      </div>
+
+      <div className='app'>
+        {/* <Hero /> */}
+        {/* <Demo /> */}
+      </div>
+    </main>
     </>
   )
 }
